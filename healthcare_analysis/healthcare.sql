2e493e4677a4bb162f9caff8378ae49d599bdd63
ReiEmRo <reiemanuelr@gmail.com>
2023-11-05 00:25:38 -0600

    New queries added.

--- healthcare_analysis/healthcare.sql
+++ healthcare_analysis/healthcare.sql
@@ -1,5 +1,14 @@
 select * from healthcare_dataset;
-
 select distinct(`Medical Condition`) from healthcare_dataset;
 
-select distinct(Hospital) from healthcare_dataset;
+# Returns the amount of clients for each Insurance Provider
+select `Insurance Provider`, count(`Insurance Provider`) 'Number of Clients' from healthcare_dataset
+group by `Insurance Provider`;
+
+# Queries all the patients whose test results were inconclusive for the month of December 2022
+select * from healthcare_dataset 
+where `Test Results` = 'Inconclusive' and 
+`Date of Admission` between '2022-12-01' and '2022-12-31'
+order by `Date of Admission`;
+
+
