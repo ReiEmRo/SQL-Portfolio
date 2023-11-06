af4c00aa4c5528e0b21677c2f722fe88ab70d5be
ReiEmRo <reiemanuelr@gmail.com>
2023-11-06 00:09:29 -0700

    Added more queries+

--- healthcare_analysis/healthcare.sql
+++ healthcare_analysis/healthcare.sql
@@ -11,4 +11,17 @@ where `Test Results` = 'Inconclusive' and
 `Date of Admission` between '2022-12-01' and '2022-12-31'
 order by `Date of Admission`;
 
+# Queries all the patients who have stayed for 30 days or more in the year 2022
+select Name, Age, `Medical Condition`, `Date of Admission`, `Discharge Date`, datediff(`Discharge Date`, `Date of Admission`) 'Days Stayed' from healthcare_dataset
+where datediff(`Discharge Date`, `Date of Admission`) >= 30 and year(`Date of Admission`) = 2022;
+
+# Queries a summary of the patients who have been admitted more than once in 2022
+with timesAdmitted as (
+	select Name, count(*) 'Times Admitted' from healthcare_dataset
+    where year(`Date of Admission`) = 2022
+    group by Name
+)
+select Name, `Medical Condition`, `Date of Admission`, `Test Results` from healthcare_dataset
+where Name in (select Name from timesAdmitted where `Times Admitted` >= 2) and year(`Date of Admission`) = 2022
+order by Name, `Date of Admission`;
 
