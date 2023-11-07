3417d3e47a4cc10374c7b753659fecc1898d2b2d
ReiEmRo <reiemanuelr@gmail.com>
2023-11-06 23:16:40 -0700

    Modified one query.

--- healthcare_analysis/healthcare.sql
+++ healthcare_analysis/healthcare.sql
@@ -1,7 +1,7 @@
 select * from healthcare_dataset;
 select distinct(`Medical Condition`) from healthcare_dataset;
 
-# Returns the amount of clients with each Insurance Provider
+# Returns the amount of clients for each Insurance Provider
 select `Insurance Provider`, count(`Insurance Provider`) 'Number of Clients' from healthcare_dataset
 group by `Insurance Provider`;
 
@@ -14,14 +14,6 @@ order by `Date of Admission`;
 # Queries all the patients who have stayed for 30 days or more in the year 2022
 select Name, Age, `Medical Condition`, `Date of Admission`, `Discharge Date`, datediff(`Discharge Date`, `Date of Admission`) 'Days Stayed' from healthcare_dataset
 where datediff(`Discharge Date`, `Date of Admission`) >= 30 and year(`Date of Admission`) = 2022;
-
-# Percent of Patients admitted with Diabetes or Cancer in 2022
-with overall2022 as (
-	select * from healthcare_dataset
-    where year(`Date of Admission`) = 2022
-)
-select round((select count(*) from overall2022
-	where `Medical Condition` in ('Diabetes', 'Cancer')) / (select count(*) from overall2022) * 100, 2) 'Percent of Cancer/Diabetes';
 
 # Queries a summary of the patients who have been admitted more than once in 2022
 with timesAdmitted as (
@@ -46,5 +38,17 @@ select * from roomUsesPerYear
 where (Year, `Times Occupied`) in (select * from maxUse)
 order by Year;
 
-
+# Percent of Patients admitted with Diabetes or Cancer for each year
+with DCPerYear as (
+	select year(`Date of Admission`) 'Year', count(`Medical Condition`) 'DC count' from healthcare_dataset
+    where `Medical Condition` in ('Diabetes', 'Cancer')
+    group by year(`Date of Admission`)
+),
+overallPerYear as (
+	select year(`Date of Admission`) 'Year', count(`Medical Condition`) 'Overall' from healthcare_dataset
+    group by year(`Date of Admission`)
+)
+select DCPerYear.Year, round(`DC Count`/`Overall` * 100, 2) '% of patients with Diabetes/Cancer' from DCPerYear
+join overallPerYear on DCPerYear.year = overallPerYear.year
+order by DCPerYear.year;
 
